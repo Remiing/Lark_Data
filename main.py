2a43a34544814465e8ac818a448623a9113a17b1
Remiing <38000675+Remiing@users.noreply.github.com>
2022-11-14 19:12:34 +0900

    try_num float to int

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def calc(probability):
     success_this_time = 0   # N트만에 성공할 확률
     cumulative = 0          # N트안에 성공할 확률
     master_energy = 0       # 장인의 기운
-    num = 1                 # 트라이 횟수
+    try_num = 1                 # 트라이 횟수
 
     while True:
-        if num <= 11:
-            change_probability = round(probability + (probability * 0.1 * (num-1)), 5)
+        if try_num <= 11:
+            change_probability = round(probability + (probability * 0.1 * (try_num-1)), 5)
         if master_energy >= 1:
             change_probability, master_energy = 1, 1
         success_this_time = round(fail_before * change_probability, 5)
@@ -22,19 +22,20 @@ def calc(probability):
         if cumulative >= 1:
             cumulative = 1
         # print(f'{num} | {change_probability} | {success_this_time} | {cumulative} | {master_energy}')     # 강화 버튼 누르는 시점
-        data = {'num': int(num), 'change_probability': change_probability, 'success_this_time': success_this_time, 'cumulative': cumulative, 'master_energy': master_energy}
+        data = {'try_num': try_num, 'change_probability': change_probability, 'success_this_time': success_this_time, 'cumulative': cumulative, 'master_energy': master_energy}
         try_df = try_df.append(data, ignore_index=True)
         if master_energy >= 1: break
         fail_before = round(fail_before * (1 - change_probability), 5)
         master_energy = round(master_energy + change_probability * master_energy_fixed_value, 5)
-        num += 1
+        try_num += 1
 
     # 평균 = 1*0.1500 + 2*0.1402 + 3*0.1278 ... num * success_this_time
-    avg = 0
-    for i, prob in zip(try_df['num'], try_df['success_this_time']):
-        avg += i*prob
-    #print(avg)
+    # avg = 0
+    # for i, prob in zip(try_df['num'], try_df['success_this_time']):
+    #     avg += i*prob
+    # print(avg)
 
+    try_df = try_df.astype({'try_num': int})
     return try_df
 
 
